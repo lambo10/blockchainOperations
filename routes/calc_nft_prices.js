0e7f7ab8fe6f7943c9b49cf56e72d81ca43099b4
Lambert Nnadi <nnadi406@gmail.com>
2022-09-13 15:31:12 +0100

    new commit

--- routes/calc_nft_prices.js
+++ routes/calc_nft_prices.js
@@ -2,14 +2,12 @@ const express = require("express");
 const Web3 = require("web3");
 const router = express.Router();
 const authenticator = require("../authenticator/index.js");
+const nftAmounts = [90000, 30000, 2400000, 2400000, 60000, 60000, 48000, 48000, 18000, 30000, 90000, 30000, 5400, 9000, 7200, 120000, 180000, 7200, 240000, 120000, 4800, 5400, 4800, 30000, 120000, 30000, 240000, 480000, 120000, 60000]
 
 function calcNftPrices(k) {
     // recommended k = 18000
     result = [];
-    nftAmounts = [45000, 15000, 1200000, 1200000, 30000, 30000, 24000, 24000, 9000, 15000, 45000, 15000,
-        2700, 4500, 3600, 60000, 90000, 3600, 120000, 60000, 2400, 2700, 2400, 15000, 60000,
-        15000, 120000, 240000, 60000, 30000
-    ];
+
     nftAmounts.forEach(e => {
         result.push(k / e);
     });
@@ -25,12 +23,32 @@ function ethPricesInWEI(prices) {
     return result;
 }
 
+function calctotalBNBamountForEachNFT(prices) {
+    result = [];
+    count = 0;
+    prices.forEach(e => {
+        result.push(e * nftAmounts[count]);
+        count++;
+    });
+    return result;
+}
+
+function calcTotalAmount(prices) {
+    result = 0;
+    prices.forEach(e => {
+        result = result + e;
+    });
+    return result;
+}
+
 router.get("/", (req, res) => {
     if (authenticator.auth(req.query.apiKey)) {
         _prices = calcNftPrices(req.query.k);
         res.json({
             prices: _prices,
-            inWei: ethPricesInWEI(_prices)
+            inWei: ethPricesInWEI(_prices),
+            totalBNBamountForEachNFT: calctotalBNBamountForEachNFT(prices),
+            totalAmounnt: calcTotalAmount(prices)
         });
     } else {
         res.json({
