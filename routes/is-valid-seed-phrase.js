e7f571e8ab673dc06f351d97c414c21dd6a96ac3
Lambert Nnadi <nnadi406@gmail.com>
2022-09-21 11:26:24 +0100

    new commit

--- routes/is-valid-seed-phrase.js
+++ routes/is-valid-seed-phrase.js
@@ -1,20 +1,14 @@
 const express = require("express");
 const ethers = require("ethers");
 const router = express.Router();
-const authenticator = require("../authenticator/index.js");
 
 
 router.get("/", (req, res) => {
-    if (authenticator.auth(req.query.apiKey)) {
-        let mnemonic = req.query.mnemonic;
-        let isValid = ethers.utils.isValidMnemonic(mnemonic);
-        res.json({ isValid });
-    } else {
-        res.json({
-            success: false,
-            msg: "Invalid API key",
-        });
-    }
+
+    let mnemonic = req.query.mnemonic;
+    let isValid = ethers.utils.isValidMnemonic(mnemonic);
+    res.json({ isValid });
+
 });
 
 module.exports = router;
