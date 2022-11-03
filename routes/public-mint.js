bf8d57a2ab70bffe0b47112161cf4172584e9f18
Lambert Nnadi <nnadi406@gmail.com>
2022-11-03 06:05:25 +0100

    new commit

--- routes/public-mint.js
+++ routes/public-mint.js
@@ -47,7 +47,7 @@ router.get(
 
                 const options = {
                     value: ethers.utils.parseEther(req.query.cost),
-                    gasLimit: 3e5,
+                    gasLimit: 5e5,
                 }
 
                 let tx = await contractWithSigner.mint(req.query.id, req.query.amount, options);
