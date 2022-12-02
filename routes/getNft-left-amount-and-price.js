caa352417c99e8c475cae91d9d85dc582193681f
Lambert Nnadi <nnadi406@gmail.com>
2022-12-02 15:52:10 +0100

    new commit

--- routes/getNft-left-amount-and-price.js
+++ routes/getNft-left-amount-and-price.js
@@ -48,16 +48,15 @@ router.get(
                 let contract = new ethers.Contract(nft_contract_address, erc1155Abi, provider);
 
                 let maxNftsAmount = await contract.maxNftsAmount(req.query.id);
-                let maxNftsAmount_json = ethers.utils.formatEther(maxNftsAmount);
+                let maxNftsAmount_json = JSON.parse(maxNftsAmount);
 
                 let mintedNftsAmount = await contract.mintedNftsAmount(req.query.id);
-                let mintedNftsAmount_json = ethers.utils.formatEther(mintedNftsAmount);
+                let mintedNftsAmount_json = JSON.parse(mintedNftsAmount);
 
                 let price = await contract.nftMintPrice(req.query.id);
                 let price_fe = ethers.utils.formatEther(price);
 
                 let price_in_bnb = price_fe / parseFloat(await tokenPrices.calcBNBPrice());
-
 
                 res.json({
                     amountLeft: (maxNftsAmount_json - mintedNftsAmount_json),
